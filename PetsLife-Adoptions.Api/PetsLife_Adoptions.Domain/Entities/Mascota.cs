9f32c9c86806485d4c110fa7b22aa330c3d5c57c
German Obregon <germanobregon2@gmail.com>
2021-04-22 19:45:07 -0300

    entities en dev ahora si

--- PetsLife-Adoptions.Api/PetsLife_Adoptions.Domain/Entities/Mascota.cs
+++ PetsLife-Adoptions.Api/PetsLife_Adoptions.Domain/Entities/Mascota.cs
@@ -7,18 +7,14 @@ namespace PetsLife_Adoptions.Domain.Entities
     public class Mascota
     {
         public int MascotaId { get; set; }
-<<<<<<< HEAD
+
         public int AnimalId { get; set; }
         public string Imagen { get; set; }
         public string Nombre { get; set; }
         public int Edad { get; set; }
         public double Peso { get; set; }
-=======
-        public string Nombre { get; set; }
-        public int Edad { get; set; }
-        public double Peso { get; set; }
-        public string Imagen { get; set; }
->>>>>>> entities
+
+       
         public List<AdoptanteMascota> AdoptanteMascota { get; set; }
     }
 }
