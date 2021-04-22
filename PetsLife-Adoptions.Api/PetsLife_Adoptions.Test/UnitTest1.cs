8e751bd4e8f749bab7ff7fa980956383d7ef5c98
Jose Luis Bravo <bravo.jose.luis18@gmail.com>
2021-04-22 13:09:30 -0300

    add git ignore, add test project, change version sdk

---
+++ PetsLife-Adoptions.Api/PetsLife_Adoptions.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PetsLife_Adoptions.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
